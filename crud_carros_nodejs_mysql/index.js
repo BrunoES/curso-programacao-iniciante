3fbbd57be099100aab068d5c1ceb8ce3a6e6fe49
Bruno Henrique <brunoh915@gmail.com>
2022-06-12 18:33:02 -0300

    Implementação inicial de Restify em servidor nodejs CRUD CARS.

--- crud_carros_nodejs_mysql/index.js
+++ crud_carros_nodejs_mysql/index.js
@@ -1,12 +1,34 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const restify = require('restify');
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+  host: "localhost",
+  user: "root",
+  password: "1234"
+});
+
+const server = restify.createServer({
+  name: 'crud-carros-server',
+  version: '1.0.0'
+});
+
+server.use(
+  function crossOrigin(req,res,next){
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      return next();
+    }
+);
+
+server.get('/carros', function (req, res, next) {
+  res.send({"resposta": "conteúdo"});
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
